a980cf1f0b120070d9b4eb397cda48b0b74e8992
Kamryn Furuichi <kamsterhd12@gmail.com>
2026-09-23 20:45:56 -0700

    Player Controls established (WASD and Mouse Controls)

---
+++ scripts/player/player.gd
@@ -0,0 +1,35 @@
+extends CharacterBody3D
+
+@onready var camera = $Camera3D
+
+const MOUSE_SENSITIVITY = 0.002
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump. ui_accept usually means Space or Enter keys
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+		camera.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
